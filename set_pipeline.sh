cb2aa007a18c8981241dbf31beba2f1be6815e73
Scott Paige <scottpaige1@gmail.com>
2020-02-11 10:20:36 -0700

    Changed param naming

--- set_pipeline.sh
+++ set_pipeline.sh
@@ -7,17 +7,17 @@ echo "Concourse login ..."
 ruby "/fly_login.rb" \
       "${CONCOURSE_USERNAME}" \
       "${CONCOURSE_PASSWORD}" \
-      "${CONCOURSE_ENV}" \
+      "${CONCOURSE_TEAM}" \
       "${CONCOURSE_URL}" \
-      "${CONCOURSE_ENV}"
+      "${CONCOURSE_TEAM}"
 
-fly -t ${CONCOURSE_ENV} sync
+fly -t ${CONCOURSE_TEAM} sync
 
 echo "Creating or Updating pipeline .. ${PIPELINE_NAME}"
 
-fly --target ${CONCOURSE_ENV} set-pipeline \
+fly --target ${CONCOURSE_TEAM} set-pipeline \
       --pipeline "${PIPELINE_NAME}" \
       --config $PIPELINE_CONFIG \
       --non-interactive
 
-fly --target ${CONCOURSE_ENV} unpause-pipeline --pipeline "${PIPELINE_NAME}"
+fly --target ${CONCOURSE_TEAM} unpause-pipeline --pipeline "${PIPELINE_NAME}"
