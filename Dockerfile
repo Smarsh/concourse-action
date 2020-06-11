d8a07954e3ccc9d7a56f1a967e0f6519a2e9c5b7
Scott Paige <scottpaige1@gmail.com>
2020-06-11 13:03:44 -0700

    Added ability to use fly6

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,13 @@ FROM debian
 
 RUN apt-get update && apt-get install -y wget
 
+RUN wget https://github.com/concourse/concourse/releases/download/v6.2.0/fly-6.2.0-linux-amd64.tgz && \
+    tar xf fly-6.2.0-linux-amd64.tgz -C /usr/local/bin/ && \
+    mv /usr/local/bin/fly /usr/local/bin/fly6 && \
+    chmod +x /usr/local/bin/fly6
+
 RUN wget https://github.com/concourse/concourse/releases/download/v5.8.0/fly-5.8.0-linux-amd64.tgz && \
-    tar xf fly*.tgz -C /usr/local/bin/ && \
+    tar xf fly-5.8.0-linux-amd64.tgz  -C /usr/local/bin/ && \
     chmod +x /usr/local/bin/fly
 
 RUN wget https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.7.0/credhub-linux-2.7.0.tgz && \
--- set_pipeline.sh
+++ set_pipeline.sh
@@ -12,29 +12,56 @@ fi
 CONCOURSE_PASSWORD="$(credhub get -q -n /concourse/${CONCOURSE_TEAM}/ci-user-password)"
 
 echo "Concourse login ..."
+if [ -z $FLY_6 ]; then
+  fly --target "${CONCOURSE_TEAM}" login \
+    --concourse-url "${CONCOURSE_URL}" \
+    --team-name "${CONCOURSE_TEAM}" \
+    --username "${CONCOURSE_USERNAME}" \
+    --password "${CONCOURSE_PASSWORD}"
 
-fly --target "${CONCOURSE_TEAM}" login \
-  --concourse-url "${CONCOURSE_URL}" \
-  --team-name "${CONCOURSE_TEAM}" \
-  --username "${CONCOURSE_USERNAME}" \
-  --password "${CONCOURSE_PASSWORD}"
 
+  fly -t ${CONCOURSE_TEAM} sync
 
-fly -t ${CONCOURSE_TEAM} sync
+  if [ -z $VARIABLE_FILE ]; then
+    fly --target "${CONCOURSE_TEAM}" set-pipeline \
+      --pipeline "${PIPELINE_NAME}" \
+      --config "${PIPELINE_CONFIG}" \
+      --non-interactive
 
-if [ -z $VARIABLE_FILE ]; then
-  fly --target "${CONCOURSE_TEAM}" set-pipeline \
-    --pipeline "${PIPELINE_NAME}" \
-    --config "${PIPELINE_CONFIG}" \
-    --non-interactive
+    fly --target "${CONCOURSE_TEAM}" unpause-pipeline --pipeline "${PIPELINE_NAME}"
+  else
+    fly --target "${CONCOURSE_TEAM}" set-pipeline \
+      --pipeline "${PIPELINE_NAME}" \
+      --config "${PIPELINE_CONFIG}" \
+      --load-vars-from "${VARIABLE_FILE}"
+      --non-interactive
 
-  fly --target "${CONCOURSE_TEAM}" unpause-pipeline --pipeline "${PIPELINE_NAME}"
+    fly --target "${CONCOURSE_TEAM}" unpause-pipeline --pipeline "${PIPELINE_NAME}"
+  fi
 else
-  fly --target "${CONCOURSE_TEAM}" set-pipeline \
-    --pipeline "${PIPELINE_NAME}" \
-    --config "${PIPELINE_CONFIG}" \
-    --load-vars-from "${VARIABLE_FILE}"
-    --non-interactive
+  fly6 --target "${CONCOURSE_TEAM}" login \
+    --concourse-url "${CONCOURSE_URL}" \
+    --team-name "${CONCOURSE_TEAM}" \
+    --username "${CONCOURSE_USERNAME}" \
+    --password "${CONCOURSE_PASSWORD}"
 
-  fly --target "${CONCOURSE_TEAM}" unpause-pipeline --pipeline "${PIPELINE_NAME}"
+
+  fly6 -t ${CONCOURSE_TEAM} sync
+
+  if [ -z $VARIABLE_FILE ]; then
+    fly6 --target "${CONCOURSE_TEAM}" set-pipeline \
+      --pipeline "${PIPELINE_NAME}" \
+      --config "${PIPELINE_CONFIG}" \
+      --non-interactive
+
+    fly6 --target "${CONCOURSE_TEAM}" unpause-pipeline --pipeline "${PIPELINE_NAME}"
+  else
+    fly6 --target "${CONCOURSE_TEAM}" set-pipeline \
+      --pipeline "${PIPELINE_NAME}" \
+      --config "${PIPELINE_CONFIG}" \
+      --load-vars-from "${VARIABLE_FILE}"
+      --non-interactive
+
+    fly6 --target "${CONCOURSE_TEAM}" unpause-pipeline --pipeline "${PIPELINE_NAME}"
+  fi
 fi
